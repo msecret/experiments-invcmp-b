78a03eeadb1766c9bf8a08b5318875593b128d96
Marco Secreto <msegreto@miceover.com>
2014-06-13 15:27:43 -0700

    Define investment backend model and frontend schema
    Fixes #23
    
    The schema is being defined in the frontend app under
    /app/schema/investment.json. Its definition is not committed as of this moment.

---
+++ model/investment.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+type (
+	// Investments is a list of Investment(s).
+	Investments []Investment
+
+	// Investment models a single entity on the stock market searchable by
+	// its symbol. It may have a group associated with it. It's fields is a
+	// investment schema:
+	//   /public/schema/investment.json
+	Investment struct {
+		Base
+		Id     bson.ObjectId `json:"id" bson:"_id"`
+		Group  Group         `json:"group" bson:"group"`
+		Fields bson.M        `json:"fields" bson:"fields"`
+	}
+	// InvestmentRepo is responsible for all actions on the database related to the
+	// Investment model
+	InvestmentRepo struct {
+		Collection *mgo.Collection
+	}
+)
