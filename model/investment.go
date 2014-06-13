0aa51e5592ce13249030524c647cafe7f28b0ee6
Marco Secreto <msegreto@miceover.com>
2014-06-13 15:52:34 -0700

    Create investment model
    Fixes #15
    
    TODO:
    - Need unique index on symbol field. Can use mgo EnsureIndex for this
    but unsure of where to initialize this function.

--- model/investment.go
+++ model/investment.go
@@ -16,6 +16,7 @@ type (
 	Investment struct {
 		Base
 		Id     bson.ObjectId `json:"id" bson:"_id"`
+		Symbol string        `json:"symbol" bson:"symbol"`
 		Group  Group         `json:"group" bson:"group"`
 		Fields bson.M        `json:"fields" bson:"fields"`
 	}
@@ -25,3 +26,16 @@ type (
 		Collection *mgo.Collection
 	}
 )
+
+// CreateOne will attempt to update the base time fields and then insert one
+// Investment into the database by taking a Investment to be created. Returns
+// the created investment.
+func (r *InvestmentRepo) CreateOne(toCreate Investment) (Investment, error) {
+	toCreate.Create()
+	err := r.Collection.Insert(toCreate)
+	if err != nil {
+		return Investment{}, err
+	}
+
+	return toCreate, nil
+}
